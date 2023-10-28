457744cbdd83e002889ba88ee2d91a3499fcbb95
Stephen Chang <sjchang@Stephens-MacBook-Air.local>
2023-10-28 16:28:20 -0700

    memesocket

---
+++ memesocket.py
@@ -0,0 +1,39 @@
+import asyncio
+import websockets
+import pyaudio
+
+CHUNK = 50000  # Number of audio frames per buffer
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+
+# Define a function to handle WebSocket connections
+async def audio_stream(websocket, path):
+    # Print a message when a client connects
+    print(f"Client connected from {websocket.remote_address}")
+
+    audio = pyaudio.PyAudio()
+
+    try:
+        stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+        while True:
+            data = await websocket.recv()
+            stream.write(data)
+            print("audio recieved")
+    except websockets.exceptions.ConnectionClosedError:
+        pass  # Connection closed by the client
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        if 'stream' in locals() and stream.is_active():
+            stream.stop_stream()
+            stream.close()
+        if 'audio' in locals():
+            audio.terminate()
+
+# Set up the WebSocket server
+start_server = websockets.serve(audio_stream, "localhost", 8765)
+
+# Start the server
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
