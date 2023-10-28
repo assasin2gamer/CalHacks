1f9543171dab65ea487f6533e69dcb77415a5a5f
Stephen Chang <sjchang@Stephens-MacBook-Air.local>
2023-10-28 16:39:54 -0700

    mems

--- memesocket.py
+++ memesocket.py
@@ -1,41 +1,45 @@
-from flask import Flask, request, jsonify
-import threading
-import time
+import asyncio
+import websockets
 
-app = Flask(__name__)
+# Dictionary to store client WebSocket connections
+clients = set()
 
-# Define a global variable to store data
-data_store = []
+# Define a function to handle WebSocket connections
+async def handle_connection(websocket, path):
+    # Add the client to the set of connected clients
+    clients.add(websocket)
+    print(f"Client connected from {websocket.remote_address}")
 
-# Function to process data when it is received
-def process_data(data):
-    # You can define your data processing logic here
-    print(f"Received data: {data}")
+    try:
+        async for message in websocket:
+            # Check if the received message is a single data point
+            if message.startswith("SINGLE:"):
+                # Process single data point
+                single_data = message[len("SINGLE:"):]
+                print(f"Received single data: {single_data}")
+                # Handle the single data as needed
 
-# Route for receiving continuous data
-@app.route('/send_data', methods=['POST'])
-def send_data():
-    data = request.json
-    if data:
-        data_store.append(data)
-        return jsonify({"message": "Data received successfully"}), 200
-    else:
-        return jsonify({"message": "Invalid data"}), 400
+            # Check if the received message is continuous data
+            elif message.startswith("CONTINUOUS:"):
+                # Process continuous data
+                continuous_data = message[len("CONTINUOUS:"):]
+                print(f"Received continuous data: {continuous_data}")
+                # Handle the continuous data as needed
 
-# Function to continuously send data
-def send_continuous_data():
-    while True:
-        # Simulate data generation (you can replace this with your data source)
-        data_point = {"value": time.time()}
-        # Send data to the server
-        response = requests.post('http://localhost:5000/send_data', json=data_point)
-        if response.status_code == 200:
-            print("Continuous data sent successfully")
-        time.sleep(1)  # Adjust the interval as needed
+            else:
+                print(f"Received unrecognized message: {message}")
 
-# Create a thread for sending continuous data
-continuous_data_thread = threading.Thread(target=send_continuous_data)
-continuous_data_thread.start()
+    except websockets.exceptions.ConnectionClosedError:
+        pass  # Connection closed by the client
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        # Remove the client from the set when they disconnect
+        clients.remove(websocket)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Set up the WebSocket server
+start_server = websockets.serve(handle_connection, "localhost", 8765)
+
+# Start the server
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
